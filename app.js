98c55d09783a505fd0652f8a8d2996bbe8409964
ananth <ananthupadrasta1234@gmail.com>
2019-05-24 21:20:42 +0530

    newly modified

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 (function () {
   'use strict';
 
-angular.module('myFirstApp', [])
+angular.module('NameCalculator', [])
 
-.controller('myFirstController', function ($scope) {
- $scope.name = "ananthrocz"
+.controller('NameCalculatorController', function ($scope) {
+ $scope.name="";
+ $scope.totalvalue=0;
+
+ $scope.displayNumeric=function () {
+   var totalNamevalue = CalculatNumericForString($scope.name);
+     $scope.totalvalue = totalNamevalue;
+   };
 });
+function CalculatNumericForString(string) {
+  var totalStringvalue = 0;
+  for(var i=0;i<string.length;i++) {
+    totalStringvalue + = string.charAt(i);
+  }
+  return totalStringvalue;
+}
+
 
 })();
