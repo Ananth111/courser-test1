864cfb63e470a19550ec0b9f1be067112763a775
ananth <ananthupadrasta1234@gmail.com>
2019-06-02 20:04:01 +0530

    updated

--- app1.js
+++ app1.js
@@ -1,18 +1,18 @@
-//(function () {
-  //'use strict';
+// (function () {
+//   'use strict';
+//
+// angular.module('DIApp', [])
+// .controller('DIController', ['$scope', '$filter',DIController]);
+//
+// function DIController($scope, $filter) {
+//   $scope.name = "Ananth";
+//
+//   $scope.upper = function () {
+//    var upCase = $filter('uppercase');
+//    $scope.name = upCase($scope.name);
+//   };
+// }
+//
+// })();
 
-//angular.module('DIApp', [])
-//.controller('DIController', DIController);
-
-//function DIController($scope, $filter) {
-  //$scope.name = "Ananth";
-
-  //$scope.upper = function () {
-   //var upCase = $filter('uppercase');
-   //$scope.name = upCase($scope.name);
-  //};
-//}
-
-//})();
-
-!function(){"use strict";angular.module("DIApp",[]).controller("DIController",function(n,e){n.name="Ananth",n.upper=function(){var r=e("uppercase");n.name=r(n.name)}})}();
+!function(){"use strict";angular.module("DIApp",[]).controller("DIController",["$scope","$filter",function(n,e){n.name="Ananth",n.upper=function(){var r=e("uppercase");n.name=r(n.name)}}])}();
